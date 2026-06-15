fa5e5de25480a1282675810aa1001f229af2fe01
odgm <ordyelizkh@gmail.com>
2026-06-15 17:01:44 -0300

    funciones en app

--- js/app.js
+++ js/app.js
@@ -8,3 +8,31 @@ const egresos = [
     new Egreso("Mercado", 400)
 ];
 
+let cargarApp = () => {
+    cargarCabecero()
+}
+
+let totalIngresos = () => {
+    let totalIngreso = 0;
+    for(let ingreso of ingresos){
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = () =>{
+    let totalEgreso = 0;
+    for(let egreso of egresos){
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
+let cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    document.getElementById("presupuesto").innerHTML = presupuesto;
+    document.getElementById("porcentaje").innerHTML = porcentajeEgreso;
+    document.getElementById("ingresos").innerHTML = totalIngresos();
+    document.getElementById("egresos").innerHTML = totalEgresos();
+}
