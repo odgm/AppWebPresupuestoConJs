75e067acb1414d8e9ddf4387293fc5f5c5220350
odgm <ordyelizkh@gmail.com>
2026-06-15 16:49:15 -0300

    escribimos el contenido de los js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const ingresos = [
+    new Ingreso("Salario", 2100.00),
+    new Ingreso("Venta Coche", 1500)
+];
+
+const egresos = [
+    new Egreso("Alquiler departamento", 900),
+    new Egreso("Mercado", 400)
+];
+
