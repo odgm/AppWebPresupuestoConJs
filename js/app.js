f277a07077811d870dc8265c8acbb3f5eae10f25
odgm <ordyelizkh@gmail.com>
2026-06-15 19:11:07 -0300

    agregar datos

--- js/app.js
+++ js/app.js
@@ -64,13 +64,21 @@ const crearIngresoHTML = (ingreso) => {
             <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)}</div>
             <div class="elemento_eliminar">
                 <button class="elemento_eliminar--btn">
-                    <ion-icon name="close-circle-outline"></ion-icon>
+                    <ion-icon name="close-circle-outline"
+                    onclick="eliminarIngreso(${ingreso.id})"></ion-icon>
                 </button>
             </div>
         </div>
     </div>
     `;
     return ingresoHTML;
+}
+
+const eliminarIngreso = (id) => {
+    let indiceEliminar = ingresos.findIndex( ingreso => ingreso.id === id );
+    ingresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarIngresos();
 }
 
 const cargarEgresos = () => {
@@ -90,11 +98,19 @@ const crearEgresoHTML = (egreso) => {
             <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor/totalEgresos())}</div>
             <div class="elemento_eliminar">
                 <button class="elemento_eliminar--btn">
-                    <ion-icon name="close-circle-outline"></ion-icon>
+                    <ion-icon name="close-circle-outline"
+                    onclick="eliminarEgreso(${egreso.id})"></ion-icon>
                 </button>
             </div>
         </div>
     </div>
     `;
     return egresoHTML;
+}
+
+const eliminarEgreso = (id) => {
+    let indiceEliminar = egresos.findIndex( egreso => egreso.id === id );
+    egresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarEgresos();
 }
